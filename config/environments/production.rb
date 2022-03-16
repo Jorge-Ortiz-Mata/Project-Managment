3dd5e29d47da5ef14b0c13f2e35f0312f6226b9a
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-16 13:45:06 -0600

    Rename heroku app.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,12 +94,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'afternoon-tundra-15270.herokuapp.com', protocol: "https" }
+  config.action_mailer.default_url_options = { host: 'yorch-project-managment.herokuapp.com', protocol: "https" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'afternoon-tundra-15270.herokuapp.com',
+  domain:               'yorch-project-managment.herokuapp.com',
   user_name:            'ortiz.mata.jorge@gmail.com',
   password:             Rails.application.credentials.google_password,
   authentication:       'plain',
