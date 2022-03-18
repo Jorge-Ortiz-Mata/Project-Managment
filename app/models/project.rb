5123fb1b4fb6997835fb883f8df1eebd3f2c6148
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-17 21:24:02 -0600

    Adding destroy_connections functions in User model.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,6 @@ class Project < ApplicationRecord
     belongs_to :user
     has_many :connections, dependent: :destroy
     has_many :workers, through: :connections
-    has_many_attached :files
-    
-    # before_destroy :destroy_action
-   
-    # def destroy_action
-    #    @article = Article.first
-    #     @article.destroy       
-    # end
+    has_many_attached :files, dependent: :destroy
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
-  has_one :profile
-  has_many :projects
+  has_one :profile, dependent: :destroy
+  has_many :projects, dependent: :destroy
+
+  before_destroy :destroy_connections
+   
+    def destroy_connections
+      user_connection = Connection.where(worker_id: self.id)    
+      user_connection.delete_all
+    end
+
 end
